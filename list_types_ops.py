ab2193228c65641650f5bf9ebf5518d684c5c9d5
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2015-08-13 07:44:06 +0000

    list known categories and classes

--- list_types_ops.py
+++ list_types_ops.py
@@ -4,7 +4,89 @@
 #     blender -b -P list_types_ops.py
 #
 # to get a list of all the undocumented UI object classes which can
-# be used for customizing the Blender user interface.
+# be used for customizing the Blender user interface. These have names
+# of the form category_class_name where “category” is an uppercase
+# category of Blender functionality, “class” is a two-uppercase-letter
+# code indicating the class of UI object, and “name” is the lowercase
+# identifying name. The following values for “category” have been observed:
+#
+#     ACTION
+#     ANIM
+#     ARMATURE
+#     BOID
+#     BONE
+#     BRUSH
+#     BUTTONS
+#     CAMERA
+#     CLIP
+#     CLOTH
+#     CONSOLE
+#     CONSTRAINT
+#     CURVE
+#     CYCLES
+#     DATA
+#     DOPESHEET
+#     DPAINT
+#     ED
+#     FILEBROWSER
+#     FILE
+#     FLUID
+#     FONT
+#     GPENCIL
+#     GRAPH
+#     GROUP
+#     IMAGE
+#     INFO
+#     LAMP
+#     LATTICE
+#     LOGIC
+#     MARKER
+#     MASK
+#     MATERIAL
+#     MBALL
+#     MESH
+#     NLA
+#     NODE
+#     OBJECT
+#     OUTLINER
+#     PAINTCURVE
+#     PAINT
+#     PALETTE
+#     PARTICLE
+#     PHYSICS
+#     POSELIB
+#     POSE
+#     PROPERTIES
+#     PTCACHE
+#     RENDERLAYER
+#     RENDER
+#     RIGIDBODY
+#     SCENE
+#     SCREEN
+#     SCRIPT
+#     SCULPT
+#     SEQUENCER
+#     SKETCH
+#     SOUND
+#     SURFACE
+#     TEXTURE
+#     TEXT
+#     TIME
+#     TRANSFORM
+#     UI
+#     USERPREF
+#     UV
+#     VIEW2D
+#     VIEW3D
+#     WM
+#     WORLD
+#
+# while the following values for “class” have been observed:
+#
+#     HT -- window header
+#     MT -- menu
+#     OT -- operator
+#     PT -- panel
 #
 # Copyright 2011 by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
 # Licensed under CC-BY <http://creativecommons.org/licenses/by/4.0/>.
